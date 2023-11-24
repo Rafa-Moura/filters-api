cf894cff4a1c23e83322a3fa66886b79c90e8dfe
rafael moura <rafaelfelipe.peixoto@gmail.com>
2023-11-24 16:33:45 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/jpa/filtersapi/FiltersApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jpa.filtersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiltersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiltersApiApplication.class, args);
+	}
+
+}
